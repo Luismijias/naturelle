f5b5901c8b5350c40febc9ceefdc745d5f5984b2
Luismijias <luismijias@gmail.com>
2025-12-26 13:09:44 -0300

    mudando vite de lugar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ErrorCode } from 'selenium-webdriver/lib/error'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,4 +12,6 @@ export default defineConfig({
     ]
   }
 })
+
+// testando comiti pelo vs-ErrorCode
 
